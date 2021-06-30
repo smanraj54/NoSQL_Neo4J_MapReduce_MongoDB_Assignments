4318787d4307b5d32afc4c69a49e2f8495eb3980
Manraj Singh <mn697903@dal.ca>
2021-06-29 23:08:23 -0300

    Assignment3

--- A3/src/main/java/DBMS_Connect.java
+++ A3/src/main/java/DBMS_Connect.java
@@ -3,8 +3,11 @@ import java.sql.*;
 public class DBMS_Connect {
 
     public static void main (String[] args) {
-        Transactions transactions = new Transactions();
-        transactions.RunTransactions();
+//        Transactions transactions = new Transactions();
+//        transactions.RunTransactions();
+
+
+
 //        Testing1 testing1 = new Testing1("adfbvfdbsfds");
 //        Thread t1 = new Thread(testing1);
 //        t1.start();
